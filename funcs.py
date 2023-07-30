146ef68740e7aca252675650381f8286e7c8321e
tamadNaBrowNie <77228447+tamadNaBrowNie@users.noreply.github.com>
2023-07-31 00:46:54 +0800

    string reading is done ig

--- funcs.py
+++ funcs.py
@@ -4,11 +4,15 @@ def getMachine(path):
         return stream
 def doString(string, machine):
     i = 0
-    while i < len(string) or flag:
+    final = False
+    while i < len(string) or not final :
         if i  < 0:
             return False
         input = string[i]
         flag= machine.read_char(input)
         if flag != 1 or flag != 0 or flag != -1:
             return False
+        final = machine.run.isFinal
+        i+=flag
+    return True
         
